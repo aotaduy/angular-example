f7ceff766dcf49c85248cd358869c2f531f8ccb0
Juan Cook <juan.cook@globant.com>
2015-11-26 17:58:42 -0300

    assignm 4

---
+++ client/app/pages/movie/movie-page.review.route.js
@@ -0,0 +1,20 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('moviesApp')
+        .config(configRouteMovieReview);
+
+    configRouteMovieReview.$inject = [
+        '$stateProvider'
+    ];
+
+    function configRouteMovieReview($stateProvider) {
+
+        $stateProvider
+            .state('movie-page.review', {
+                url: '/review',
+                templateUrl: 'app/pages/movie/movie-page.review.html'
+            });
+    }
+})();
