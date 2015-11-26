029aafab95e8c51d731d3d5f301a181abd4bb423
Juan Cook <juan.cook@globant.com>
2015-11-26 16:59:34 -0300

    assignment 3 not finished yet

--- client/app/components/movies-connector/moviesConnector.factory.js
+++ client/app/components/movies-connector/moviesConnector.factory.js
@@ -9,29 +9,80 @@
      */
     moviesConnectorFactory.$inject = [
         '$q',
-        '$http'
+        '$http',
+        'moviesConnectorCache'
     ];
 
     function moviesConnectorFactory(
         $q,
-        $http
+        $http,
+        moviesConnectorCache
     ) {
         var service = {
             cachedConfiguration: null,
             topRatedMovies: topRatedMovies,
             configuration: configuration,
             search: search,
-            movieInfo: movieInfo
+            movieInfo: movieInfo,
+            topRatedMoviesCompleteInfo: topRatedMoviesCompleteInfo
         };
 
         function topRatedMovies() {
             return $http.get('/api/movies/');
         }
-        function movieInfo(movieId) {
-            return $http.get('/api/movies/info/' + movieId);
+
+        function topRatedMoviesCompleteInfo(){
+            topRatedMovies()
+                .then(function(response){
+                    angular.forEach(response.results, function(obj){
+                        movieInfo(obj.id)
+                            .then(function(response){
+                                console.log(response)
+                            });
+                    });
+                });
+
+
         }
+
+        function movieInfo(movieId) {
+            var result = {};
+            var deferred = $q.defer();
+
+            var cacheResult = moviesConnectorCache.getCacheInfo(movieId);
+            if (cacheResult.hasCache === true){ //the query has been catched:
+                result.data = cacheResult.data.result;
+                deferred.resolve(result);
+            } else { //that query has NOT been catched:
+                $http.get('/api/movies/info/' + movieId)
+                    .then(function(response){
+                        result.data = response.data;
+                        moviesConnectorCache.addCacheInfo(movieId, result.data);
+                        deferred.resolve(result);
+                    });
+            }
+
+            return deferred.promise;
+        }
+
         function search(query) {
-            return $http.get('/api/movies/search/' + query);
+            var result = {};
+            var deferred = $q.defer();
+
+            var cacheResult = moviesConnectorCache.getCacheSearch(query);
+            if (cacheResult.hasCache === true){ //the query has been catched:
+                result.data = cacheResult.data.result;
+                deferred.resolve(result);
+            } else { //that query has NOT been catched:
+                $http.get('/api/movies/search/' + query)
+                    .then(function(response){
+                        result.data = response.data;
+                        moviesConnectorCache.addCacheSearch(query, result.data);
+                        deferred.resolve(result);
+                    });
+            }
+
+            return deferred.promise;
         }
 
         function configuration() {
