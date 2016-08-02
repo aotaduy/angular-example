aa9c347793ad1db15a871c2e72909fbc3527590f
aotaduy <aotaduy@users.noreply.github.com>
2016-08-02 19:52:43 -0300

    remove condition

--- server/api/movies/index.js
+++ server/api/movies/index.js
@@ -5,21 +5,13 @@ var express = require('express');
 var router = express.Router();
 function createRouter(app) {
 
-    if (!process.env.MOVIEDB_API) {
-        router.get('/', function(req, res){
-            res.status(404);
-            res.json({error: 404})
-        });
-    } else {
-        var controller = require('./movies.controller')(app) ;
-        router.get('/', controller.index);
-        router.get('/info/:movieId', controller.get);
-        router.get('/configuration', controller.configuration);
-        router.get('/playing', controller.nowPlaying);
-        router.get('/search/:query', controller.search);
-        router.post('/review/:movieId', controller.review);
-
-    }
+    var controller = require('./movies.controller')(app) ;
+    router.get('/', controller.index);
+    router.get('/info/:movieId', controller.get);
+    router.get('/configuration', controller.configuration);
+    router.get('/playing', controller.nowPlaying);
+    router.get('/search/:query', controller.search);
+    router.post('/review/:movieId', controller.review);
     return router;
 }
 module.exports = createRouter;
